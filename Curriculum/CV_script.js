89238be797eaf51c4a751de204a67bc8ee3f1681
NicoBarbi3 <72058564+NicoBarbi3@users.noreply.github.com>
2023-01-31 01:16:41 -0300

    Add files via upload

---
+++ Curriculum/CV_script.js
@@ -0,0 +1,47 @@
+let pCaja6 = document.getElementById("pCaja6")
+document.getElementById("botonSobreMi").addEventListener("click", function (){
+    this.classList.toggle("botones")
+    this.classList.toggle("botonSobreMiContraido")
+    pCaja6.classList.toggle("pOculto")
+    pCaja6.classList.toggle("pVisible")
+})
+
+let pCaja7 = document.getElementById("pCaja7")
+document.getElementById("botonExperiencia").addEventListener("click", function (){
+    this.classList.toggle("botones")
+    this.classList.toggle("botonSobreMiContraido")
+    pCaja7.classList.toggle("pOculto")
+    pCaja7.classList.toggle("pVisible")
+})
+
+let pCaja8 = document.getElementById("pCaja8")
+document.getElementById("botonFormacion").addEventListener("click", function (){
+    this.classList.toggle("botones")
+    this.classList.toggle("botonSobreMiContraido")
+    pCaja8.classList.toggle("pOculto")
+    pCaja8.classList.toggle("pVisible")
+})
+
+let pCaja9 = document.getElementById("pCaja9")
+document.getElementById("botonPortfolio").addEventListener("click", function (){
+    this.classList.toggle("botones")
+    this.classList.toggle("botonSobreMiContraido")
+    pCaja9.classList.toggle("pOculto")
+    pCaja9.classList.toggle("pVisible")
+})
+
+let pCaja10 = document.getElementById("pCaja10")
+document.getElementById("botonContacto").addEventListener("click", function (){
+    this.classList.toggle("botones")
+    this.classList.toggle("botonSobreMiContraido")
+    pCaja10.classList.toggle("pOculto")
+    pCaja10.classList.toggle("pVisible")
+})
+
+let pCaja11 = document.getElementById("pCaja11")
+document.getElementById("botonCvenPDF").addEventListener("click", function (){
+    this.classList.toggle("botones")
+    this.classList.toggle("botonSobreMiContraido")
+    pCaja11.classList.toggle("pOculto")
+    pCaja11.classList.toggle("pVisible")
+})
